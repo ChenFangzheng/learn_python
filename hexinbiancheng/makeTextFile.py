475a9fd6031b89e40aa52d00956e94117434e427
fangzheng <chenfangzheng@outlook.com>
2017-05-09 09:25:33 +0800

    make and read file

---
+++ hexinbiancheng/makeTextFile.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+'''create text file'''
+
+import os 
+ls=os.linesep
+
+#get file name 
+
+while True:
+    fname=raw_input("Enter your filename: ")
+    if os.path.exists(fname):
+        print "Error: '%s' already exists" % fname
+    else:
+        break
+
+#get file conent (text) lines 
+all=[]
+print "\nEnter lines ('.' by itself to quit).\n"
+
+#loop until user terminates input
+
+while True:
+    entry=raw_input('>')
+    if entry=='.':
+        break;
+    else:
+        all.append(entry)
+
+# write lines to file with proper line-ending
+fobj=open(fname,'w')
+fobj.writelines(['%s%s'% (x, ls) for x in all])
+fobj.close()
+print 'DONE!'
