29d5b37d2d9e01138f025bc0f8128b345c0849a0
Fangzheng <chenfangzheng@outlook.com>
2017-11-08 14:55:15 +0800

    custom standard num methond

--- mlearning/lassocv.py
+++ mlearning/lassocv.py
@@ -2,6 +2,7 @@ import time
 import os
 import numpy as np
 import pandas as pd
+from pandas import DataFrame
 import matplotlib.pyplot as plt
 from sklearn import preprocessing
 from sklearn import linear_model
@@ -25,25 +26,45 @@ feature_dict = dict(zip(mapping_data['features'], mapping_data['names']))
 data_clean = input_data.dropna()
 
 # 文本数据数字化
-le = preprocessing.LabelEncoder()
-le.fit(data_clean.Season)
-data_clean.loc[:, 'Season'] = le.transform(data_clean.Season)
+# le = preprocessing.LabelEncoder()
+# le.fit(data_clean.Season)
+# data_clean.loc[:, 'Season'] = le.transform(data_clean.Season)
 
 # 标准化数据
 variables = data_clean[mapping_data['features'].tolist()]
 
 predictors = data_clean.copy()
 
-for feature in mapping_data['features']:
-    predictors.loc[:, feature] = preprocessing.scale(
-        predictors[feature].astype('float64'))
+# for feature in mapping_data['features']:
+#     predictors.loc[:, feature] = preprocessing.scale(
+#         predictors[feature].astype('float64'))
 
-train_X = predictors.iloc[:-1, :-1]
+# train_X = predictors.iloc[:-1, :-1]
+# train_Y = data_clean.iloc[:-1, -1]
+# pred_X = predictors.iloc[-1, :-1]
+# 自定义标准话
+X_data = predictors.iloc[:-1, :-1]
+n_features = len(X_data.columns)
+X = np.array(X_data.values)
+y = np.array(data_clean.iloc[:-1, -1].values)
+X_pred = predictors.iloc[-1, :-1]
+
+X_standard = X.copy()
+X_pred_std = X_pred.copy()
+X_desc = X_data.describe()
+for i_feature in range(n_features):
+    mean = X_desc.iloc[1, i_feature]
+    std = X_desc.iloc[2, i_feature]
+    X_standard[:, i_feature] = (X_standard[:, i_feature] - mean) / std
+    X_pred_std[i_feature] = (X_pred[i_feature] - mean) / std
+
+
+train_X = X_standard
 train_Y = data_clean.iloc[:-1, -1]
-pred_X = predictors.iloc[-1, :-1]
+pred_X = X_pred_std
 
 # 进行lasso回归的n_folds交叉验证,选择最好的模型
-num_folds = 5
+num_folds = 20
 lasso_model = LassoCV(cv=num_folds).fit(train_X, train_Y)
 
 fig1, ax1 = plt.subplots()
@@ -90,7 +111,7 @@ fig2.savefig("Lasso_coefs.png", dpi=400)
 
 Importance = abs(coefs[:, list(alphas).index(alpha_selected)])
 importance_pairs = sorted(zip(Importance, mapping_data['names']))
-filtered_importance_pair = list(filter(lambda x: x[0] > 0, importance_pairs))
+filtered_importance_pair = list(filter(lambda x: x[0] != 0, importance_pairs))
 filtered_im_keys = list(map(lambda item: item[1], filtered_importance_pair))
 filtered_im_values = list(map(lambda item: item[0], filtered_importance_pair))
 
@@ -99,7 +120,7 @@ yticks = np.arange(len(filtered_importance_pair)) + 0.5
 ax3.barh(yticks, filtered_im_values, align='center', color='purple')
 ax3.set_yticks(yticks)
 ax3.set_yticklabels(filtered_im_keys,
-                    fontsize=14, fontproperties=chinese_font)
+                    fontsize=12, fontproperties=chinese_font)
 ax3.set_xlabel("Importance")
 ax3.set_xlim(0, max(Importance))
 ax3.axis("tight")
@@ -107,17 +128,24 @@ fig3.savefig("Lasso_Importance.png", dpi=400)
 
 # 预测结果
 pre_train, pre_test, tar_train, tar_test, = train_test_split(
-    train_X, train_Y, test_size=.3, random_state=123)
+    train_X, train_Y, test_size=.1, random_state=123)
 
-model = linear_model.Lasso(alpha_selected).fit(
-    pre_train, tar_train.reshape(-1, 1))
+model = linear_model.Lasso(alpha=Alpha_Best).fit(
+    train_X, train_Y.values.reshape(-1, 1))
 
 from sklearn.metrics import mean_squared_error
 
 Train_Error = mean_squared_error(tar_train, model.predict(pre_train))
 Test_Error = mean_squared_error(tar_test, model.predict(pre_test))
 print('Training data MSE: %6f' % Train_Error)
-print('Test data MSE: %6f' %Test_Error)
+print('Test data MSE: %6f' % Test_Error)
 
 y_pred = model.predict(pred_X.values.reshape(1, -1))
 print(y_pred)
+
+Df_Importance = DataFrame(model.coef_,
+                          columns=['value'],
+                          index=mapping_data['names'])
+Df_Importance.index.name = "features"
+Df_Importance.to_excel("Total_Lasso_Feature_Importance.xlsx",
+                       sheet_name="feature_importance")
